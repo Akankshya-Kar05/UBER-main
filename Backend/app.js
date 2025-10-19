0fb2e654398561cb6eb54e830e69b709947b2f8e
Akankshya-Kar05 <akankshyakar51@gmail.com.@github.com>
2025-10-19 18:39:35 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const cors = require('cors')
+const express = require('express')
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db')
+const userRoutes = require('./routes/user.routes')
+const captainRoutes = require('./routes/captain.routes')
+const mapsRoutes = require('./routes/maps.routes')
+
+connectToDb()
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.get('/',(req,res)=>{
+    res.send("hello11")
+});
+
+app.use('/users', userRoutes)
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes)
+
+module.exports=app;
